8762557c935474ebb1cc39d0dfe3c3456f6ee6cd
Kangmin Han <kangminpersonal@gmail.com>
2020-12-31 00:07:31 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="green-background">
+     <img src={logo} lat="logo"/>
+     <h2>Let's develop!</h2>
+
+    </div>
+  );
+}
+
+export default App;
