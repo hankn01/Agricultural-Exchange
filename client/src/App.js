4944fc2d3f2bde33eed46f64bd8a1bef03a70c12
Kangmin Han <kangminpersonal@gmail.com>
2021-01-13 17:00:24 +0900

    Title line Implemented & Certi Information Bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,17 @@ import TableCell from '@material-ui/core/TableCell'
 import { withStyles} from '@material-ui/core/styles'
 import axios from 'axios'
 import CircularProgress from '@material-ui/core/CircularProgress'
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 const styles = theme => ({
   root: {
     width: '100%',
@@ -22,7 +33,64 @@ const styles = theme => ({
   },
   progress: {
     margin: theme.spacing(2)
-  }
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white,0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white,0.25),
+
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+
+  },
+  searchIcon: {
+    width: theme.spacing(9),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+
 });
 
 
@@ -75,13 +143,24 @@ render() {
   const {classes} = this.props;
 
   return (
-    <div>
-    <Paper className={classes.root}>
+    <div  className={classes.root}>
+      <AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      프프 우리농산물 직거래 플랫폼
+    </Typography>
+    
+  </Toolbar>
+</AppBar>
+    <Paper>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell>
-              <h1>프프 우리농산물 직거래</h1>
+              
             </TableCell>
           </TableRow>
         </TableHead>
--- client/src/components/agricadd.js
+++ client/src/components/agricadd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
 
 class Agricadd extends React.Component {
 
@@ -12,7 +25,8 @@ class Agricadd extends React.Component {
             gender: '',
             origin: '',
             certi: '',
-            fileName: ''
+            fileName: '',
+            open: false
         }
     }
 
@@ -31,7 +45,8 @@ class Agricadd extends React.Component {
             gender: '',
             origin: '',
             certi: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
         //window.location.reload();
         this.props.stateRefresh();
@@ -57,7 +72,7 @@ class Agricadd extends React.Component {
         formData.append('price',this.state.price);
         formData.append('gender',this.state.gender);
         formData.append('origin',this.state.origin);
-        formData.append('certi',formData.certi);
+        formData.append('certi',this.state.certi);
         const config = {
             headers: {
                 'content-type': 'multipart/form-data'
@@ -66,23 +81,66 @@ class Agricadd extends React.Component {
         return post(url, formData, config);
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState( {
+            file: null,
+            name: '',
+            price: '',
+            gender: '',
+            origin: '',
+            certi: '',
+            fileName: '',
+            open: false
+        })
+    }
     render() {
+        const {classes}  = this.props;
         return (
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    농산물 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle>농산물 추가</DialogTitle>
+                    <DialogContent>
+                    농산물 이미지: <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                <label htmlFor="raised-button-file">
+                    <Button variant="contained" color="primary" component="span" name="file">
+                        {this.state.fileName===""?"농산물 이미지 선택" : this.state.fileName}
+                    </Button>
+                </label>
+                <br/>
+                농산물 품명: <TextField type="text" name="name" value={this.state.name} onChange={this.handleValueChange}/><br/>
+    
+                가격: <TextField type="text" name="price" value={this.state.price} onChange={this.handleValueChange}/><br/>
+        
+                암수: <TextField type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                원산지: <TextField type="text" name="origin" value={this.state.origin} onChange={this.handleValueChange}/><br/>
+                인증: <TextField type="text" name="certi" value={this.state.certi} onChange={this.handleValueChange}/><br/>
+                
+
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+            /*
             <form onSubmit={this.handleFormSubmit}>
                 <h1>거래 농산물 추가</h1>
-                농산물 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                농산물 품명: <input type="text" name="name" value={this.state.name} onChange={this.handleValueChange}/><br/>
-    
-                가격: <input type="text" name="price" value={this.state.price} onChange={this.handleValueChange}/><br/>
-        
-                암수: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                원산지: <input type="text" name="origin" value={this.state.origin} onChange={this.handleValueChange}/><br/>
-                인증: <input type="text" name="certi" value={this.state.certi} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가하기</button>
+               <button type="submit">추가하기</button>
             </form>
+            */
         )
     }
 
 }
 
-export default Agricadd;
+export default withStyles(styles)(Agricadd);
